e1b01bea7f424293c75d847914eee164f5f16333
Sergiy Yurov <urovsergej10@gmail.com>
2024-12-03 22:14:30 +0200

    Solution

--- author/models.py
+++ author/models.py
@@ -7,3 +7,15 @@ class Author(models.Model):
     pseudonym = models.CharField(max_length=64, null=True, blank=True)
     age = models.IntegerField()
     retired = models.BooleanField()
+
+    def create(self, validated_data):
+        return Author.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.pseudonym = validated_data.get("pseudonym", instance.pseudonym)
+        instance.age = validated_data.get("age", instance.age)
+        instance.retired = validated_data.get("retired", instance.retired)
+        instance.save()
+        return instance
